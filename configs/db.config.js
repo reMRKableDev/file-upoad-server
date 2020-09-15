234543ec3f6dffbb649afdcd2370c5b041b6b979
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-09-15 14:45:23 +0200

    changed configurations

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,14 +1,13 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb://mrk:remrkable93@lon5-c14-0.mongo.objectrocket.com:43658,lon5-c14-2.mongo.objectrocket.com:43658,lon5-c14-1.mongo.objectrocket.com:43658/FILE-UPLOADER DATABASE?replicaSet=4d098817c9414f909beb74dd4e8a264a",
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((x) =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
