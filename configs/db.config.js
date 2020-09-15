6e09581aaa48dbe7eda6fdc6b53c3ad37af7f3a4
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-09-15 14:43:33 +0200

    Db uri added

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-  .connect('mongodb://localhost/file-upload-example-server', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .connect(
+    "mongodb://mrk:remrkable93@lon5-c14-0.mongo.objectrocket.com:43658,lon5-c14-2.mongo.objectrocket.com:43658,lon5-c14-1.mongo.objectrocket.com:43658/FILE-UPLOADER DATABASE?replicaSet=4d098817c9414f909beb74dd4e8a264a",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
